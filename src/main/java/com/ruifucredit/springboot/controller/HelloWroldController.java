462472a46ebb632f6c39b15b3f40a5b1a1e6cb33
842399185@qq.com <842399185@qq.com>
2018-09-13 10:27:17 +0800

    连接mysql

--- src/main/java/com/ruifucredit/springboot/controller/HelloWroldController.java
+++ src/main/java/com/ruifucredit/springboot/controller/HelloWroldController.java
@@ -1,16 +1,29 @@
 package com.ruifucredit.springboot.controller;
 
 
+import com.ruifucredit.springboot.domain.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 @RestController
 public class HelloWroldController {
 
-    @RequestMapping("hello")
+    @RequestMapping("/hello")
     public String helloWorldController (){
 
         return "hello world";
+    }
+
+    @RequestMapping("/getUser")
+    public User getUser(){
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
+        String formattedDate = dateFormat.format(date);
+        User user = new User("aa1", "aa@126.com", "aa", "aa123456",formattedDate);
+        return user;
     }
 }
 
